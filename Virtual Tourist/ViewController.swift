3c34438d4f5bfb28dedc32111de101e915070eeb
John Longenecker <longenecker@icloud.com>
2016-03-01 16:57:14 -0500

    Add code to update pins if dragged.

--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -13,6 +13,9 @@ import CoreData
 class ViewController: UIViewController, MKMapViewDelegate {
     var longPress = false
     var pinArray = [LocationPin]()
+    var priorPinLocation: MKAnnotation?
+    var updatePin: LocationPin?
+    var viewLoaded = false
 
     @IBOutlet weak var map: MKMapView!
     
@@ -20,7 +23,6 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let manager = NSFileManager.defaultManager()
         let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! as NSURL
         return url.URLByAppendingPathComponent("visibleMap").path!
-        
     }
     
     var sharedContext: NSManagedObjectContext {
@@ -29,24 +31,56 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupGestureRecognizer()
+        pinArray = fetchAllPins()
+        reloadPins()
+        
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        let fileExists = checkIfFileExists()
+        if fileExists {
+            loadMapPosition()
+        } else {
+            print("Unable to load map position")
+        }
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        viewLoaded = true
+    }
+    
+    func loadMapPosition() {
+        if let setMapView = NSKeyedUnarchiver.unarchiveObjectWithFile(filePathforVisibleMap) {
+            let mapRectTest = mapRectWork(setMapView as! mapViewSize)
+            map.setVisibleMapRect(mapRectTest, animated: false)
+            print("Savedmap \(map.visibleMapRect)")
+            print("MapRectTest \(mapRectTest.origin.x), \(mapRectTest.origin.y) \(mapRectTest.size.width) \(mapRectTest.size.height)")
+            map.setVisibleMapRect(MKMapRect(origin: MKMapPoint(x: mapRectTest.origin.x, y: mapRectTest.origin.y), size: MKMapSize(width: mapRectTest.size.width, height: mapRectTest.size.height)), animated: true)
+            print("Savedmap \(map.visibleMapRect)")
+        }
+    }
+    
+    func checkIfFileExists()->Bool {
+        if NSFileManager.defaultManager().fileExistsAtPath(filePathforVisibleMap) {
+            print("File Saved")
+            return true
+        } else {
+            print("File not Saved")
+            return false
+        }
+    }
+    
+    func setupGestureRecognizer() {
         let uilgr = UILongPressGestureRecognizer(target: self, action: "addPin:")
         uilgr.minimumPressDuration = 1.3
         map.addGestureRecognizer(uilgr)
         map.delegate = self
-
-        if NSFileManager.defaultManager().fileExistsAtPath(filePathforVisibleMap) {
-            print("File Saved")
-        } else {
-            print("File not Saved")
-        }
-        
-        if let setMapView = NSKeyedUnarchiver.unarchiveObjectWithFile(filePathforVisibleMap) {
-            let mapRect = mapRectWork(setMapView as! mapViewSize)
-            map.setVisibleMapRect(mapRect, animated: true)
-        }
-        pinArray = fetchAllPins()
-        reloadPins()
     }
     
     func fetchAllPins() -> [LocationPin] {
@@ -64,29 +98,26 @@ class ViewController: UIViewController, MKMapViewDelegate {
         return MKMapRect(origin: MKMapPoint(x: mapView.originx, y: mapView.originy), size: MKMapSize(width: mapView.sizeWidth, height: mapView.sizeHeight))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     func addPin(gestureRecognizer:UILongPressGestureRecognizer){
+        //longPress is necessary to ensure that multiple pins do not drop from a single press.
         if longPress == false {
-            print("Long Press")
             let touchPoint = gestureRecognizer.locationInView(map)
             let newCoordinates = map.convertPoint(touchPoint, toCoordinateFromView: map)
             print("\(newCoordinates.latitude)")
+            
+            
             let dictionary = [
                 "latitude": newCoordinates.latitude as Double,
                 "longitude": newCoordinates.longitude as Double
             ]
+            
             let pinLocation = LocationPin(dictionary: dictionary, context: sharedContext)
             CoreDataStackManager.sharedInstance().saveContext()
-            print("Pin Location \(pinLocation.longitude)")
-            print("\(newCoordinates)")
+
             let annotation = MKPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(pinLocation.latitude), longitude: CLLocationDegrees(pinLocation.longitude))
-            print("\(annotation.coordinate)")
+
             map.addAnnotation(annotation)
             longPress = true
             
@@ -96,25 +127,46 @@ class ViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
+    
     func reloadPins() {
         for pin in pinArray {
             let annotation = MKPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(pin.latitude), longitude: CLLocationDegrees(pin.longitude))
-            print("\(annotation.coordinate)")
             map.addAnnotation(annotation)
         }
+    }
+    
+    func assignPriorPinLocation(priorPinLocation: MKAnnotation) {
+        for pin in pinArray {
+            print("Pin Lattitude \(pin.latitude) Pin \(pin.longitude)")
+            if pin.latitude == priorPinLocation.coordinate.latitude as Double && pin.longitude == priorPinLocation.coordinate.longitude {
+                print("Prior Pin Lattitude \(pin.latitude) Prior pin \(pin.longitude)")
+                updatePin = pin
+                updatePin!.longitude = priorPinLocation.coordinate.longitude
+                updatePin!.latitude = priorPinLocation.coordinate.latitude
+            }
+        }
+    }
+    
+    func updatePinLocationInCoreData(pinLocation: LocationPin, newLocation: MKAnnotation) {
+        pinLocation.latitude = newLocation.coordinate.latitude
+        pinLocation.longitude = newLocation.coordinate.longitude
+        CoreDataStackManager.sharedInstance().saveContext()
     }
     
     // MARK: - mapView Delegates
     
     func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        let visibleMap = map.visibleMapRect
+        if viewLoaded {
+            print("Region Did Change")
+            let visibleMap = map.visibleMapRect
+            print("\(visibleMap)")
         
-        let testMapViewSize = mapViewSize(originx: visibleMap.origin.x, originy: visibleMap.origin.y, sizeHeight: visibleMap.size.height, sizeWidth: visibleMap.size.width)
-        let result = NSKeyedArchiver.archiveRootObject(testMapViewSize, toFile: filePathforVisibleMap).boolValue
-        print(result)
-        
-        
+            let testMapViewSize = mapViewSize(originx: visibleMap.origin.x, originy: visibleMap.origin.y, sizeHeight: visibleMap.size.height, sizeWidth: visibleMap.size.width)
+            print("\(testMapViewSize.originx) \(testMapViewSize.originy) \(testMapViewSize.sizeWidth) \(testMapViewSize.sizeHeight)")
+            let result = NSKeyedArchiver.archiveRootObject(testMapViewSize, toFile: filePathforVisibleMap).boolValue
+            print("MapViewSaved \(result)")
+        }
     }
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
@@ -131,16 +183,25 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
     
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, didChangeDragState newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        if oldState == MKAnnotationViewDragState.Starting {
+            priorPinLocation = view.annotation
+            assignPriorPinLocation(priorPinLocation!)
+            print("annotation began at: \(priorPinLocation!.coordinate.latitude),\(priorPinLocation!.coordinate.longitude)")
+            
+        }
+        
         if newState == MKAnnotationViewDragState.Ending {
             let ann = view.annotation
             print("annotation dropped at: \(ann!.coordinate.latitude),\(ann!.coordinate.longitude)")
+            if let updatePin = updatePin, ann = ann {
+                updatePinLocationInCoreData(updatePin, newLocation: ann)
+            }
+            
         }
+        
+        
     }
     
-    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
-        //Required for updating pin location in pinArray
-        print("\(view.annotation?.coordinate.latitude) \(view.annotation?.coordinate.longitude)")
-    }
 
 }
 
